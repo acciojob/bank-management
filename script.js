102586b2a59a273200639d6a40f787907e5ecc1b
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2023-07-05 22:11:41 +0530

    ques

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+// complete all the classes according to the requirements
+
+class Bank {}
+
+class Account {}
+
+class CheckingAccount extends Account {}
+
+class SavingsAccount extends Account {}
+
+class CreditCardAccount extends Account {}
+
+// do not change this code
+window.Account = Account;
+window.CheckingAccount = CheckingAccount;
+window.SavingsAccount = SavingsAccount;
+window.CreditCardAccount = CreditCardAccount;
+window.Bank = Bank;
